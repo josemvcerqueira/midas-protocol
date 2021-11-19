63e2710baa037f8dc311bb41c1d911244e671fab
josemvcerqueira <jose@ilovemochi.com>
2021-11-19 23:51:11 +0000

    test: improve the deploy utils fn

--- lib/test-utils.ts
+++ lib/test-utils.ts
@@ -1,12 +1,15 @@
 // eslint-disable-next-line node/no-unpublished-import
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+// eslint-disable-next-line node/no-unpublished-import
 import { ethers } from 'hardhat';
 
 export const deploy = async (
   x: ReadonlyArray<string>,
-  y: Array<Array<unknown> | undefined>
+  y: Array<Array<unknown> | undefined>,
+  z: ReadonlyArray<SignerWithAddress> = []
 ): Promise<any> => {
   const contractFactories = await Promise.all(
-    x.map((name) => ethers.getContractFactory(name))
+    x.map((name, index) => ethers.getContractFactory(name, z[index]))
   );
 
   return Promise.all(
